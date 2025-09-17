4b913c2e7dca8d220c51faca294b8f05d2c4d3ff
Hikara <fnstani2019@gmail.com>
2025-09-17 12:29:32 -0400

    add comments on accessToken, refreshToken and authenticateToken

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.post("/login", (req, res) => {
 })
 
 function authenticateToken(req, res, next) {
+    // authenticate if the token is valid and secure
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(" ")[1]
     if (token == null) return res.sendStatus(401)
